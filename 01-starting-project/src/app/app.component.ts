96ad57a210915f628c17b5490dab34f68af1579d
Ornela <orneladodaj.work@gmail.com>
2025-03-25 16:46:51 +0100

    worked with inputs

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
+import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-  onCalculateInvestmentResults( data: {
-    initialInvestment: number,
-     duration: number, 
-     expectedReturn: number, 
-     annualInvestment: number}) {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+}[];
+  onCalculateInvestmentResults( data: InvestmentInput) {
       const {initialInvestment, annualInvestment, duration, expectedReturn} = data;
       const annualData = [];
       let investmentValue = initialInvestment;
@@ -33,8 +39,6 @@ export class AppComponent {
         totalAmountInvested: initialInvestment + annualInvestment * year,
       });
     }
-    console.log(annualData);
-  
-    return annualData;
+    this.resultsData = annualData;
   }
 }
